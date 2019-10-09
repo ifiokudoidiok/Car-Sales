8f76ccb31f7400fcc2165d6951da9086ba490fca
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-09 21:30:46 +0100

    imported redux libs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import * as reducers from './state/reducers';
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+
+const monsterReducer = combineReducers({
+    car: reducers.carReducer,
+    additionalPrice: reducers.additionalPriceReducer,
+    store: reducers.storeReducer,
+});
+
+const store = createStore(
+    monsterReducer,
+)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+    <App /> 
+    </Provider>,
+    rootElement);
