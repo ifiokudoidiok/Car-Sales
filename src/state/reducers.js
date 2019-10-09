56e7783e31a92fdb018b417491a3b7601a26fa38
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-09 21:32:24 +0100

    reducers set up

---
+++ src/state/reducers.js
@@ -0,0 +1,44 @@
+import * as types from "./actionTypes";
+
+const additionalPrice = 0;
+const car = {
+  price: 26395,
+  name: "2019 Ford Mustang",
+  image:
+    "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
+  features: []
+};
+const store = [
+  { id: 1, name: "V-6 engine", price: 1500 },
+  { id: 2, name: "Racing detail package", price: 1500 },
+  { id: 3, name: "Premium sound system", price: 500 },
+  { id: 4, name: "Rear spoiler", price: 250 }
+];
+
+export function carReducer(state = car, action) {
+  switch (action.type) {
+      case types.ADD_FEATURE:
+          return {
+              ...state, features: [...state.features, action.payload]
+          }
+        case types.REMOVE_FEATURE:
+            return {
+                
+            }
+    default:
+      return state;
+  }
+}
+export function additionalPriceReducer(state = additionalPrice, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export function storeReducer(state =store, action) {
+    switch (action.type) {
+      default:
+        return state;
+    }
+  }
