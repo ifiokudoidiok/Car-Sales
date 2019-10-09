f26e976133d3d968a02c021c379be1032132fc9f
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-09 21:53:34 +0100

    added  removeFeature functionality

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import * as actionCreators from '../state/actionCreators';
 
-const AddedFeature = props => {
+export const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick={e => props.removeFeature(props.feature)}>X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+export default connect(
+  state => state,
+  actionCreators
+)(AddedFeature);
