f1e5c1a191caaded70d9ce85b1ca45235f797f7f
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-09 22:38:56 +0100

    clean up

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import * as actionCreators from '../state/actionCreators';
+import React from "react";
+import { connect } from "react-redux";
+import * as actionCreators from "../state/actionCreators";
 
 export const AdditionalFeature = props => {
-  debugger
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button" onClick={e => props.addFeature(props.feature)}>Add</button>
+      <button className="button" onClick={e => props.addFeature(props.feature)}>
+        Add
+      </button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
